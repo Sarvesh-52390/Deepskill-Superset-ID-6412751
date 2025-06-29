173dd55dd2467ede3fbe2373c07bf200786d86a6
Sarvesh-52390 <sarvesh.p.s52390@gmail.com>
2025-06-29 19:05:03 +0530

    junit res

--- 727822tuec205-sarvesh.p.s/week2/JUnit_Testing/Exercises 1,3,4/src/AAAPattern/bankAccountTest.java
+++ 727822tuec205-sarvesh.p.s/week2/JUnit_Testing/Exercises 1,3,4/src/AAAPattern/bankAccountTest.java
@@ -2,8 +2,8 @@ package AAAPattern;
 import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
+
 public class bankAccountTest {
 	private bankAccount account;
 
